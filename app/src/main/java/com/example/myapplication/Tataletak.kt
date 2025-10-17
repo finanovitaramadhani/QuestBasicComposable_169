1ad7c9688c5f5d32e467d4a302788a7e4d536267
finanovita <finanovita479@gmail.com>
2025-10-17 11:42:49 +0700

    Menambahkan layout Row kolom 2

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -94,4 +94,12 @@ fun TataletakRowColumn(modifier: Modifier) {
             Text(text = "Komponen2Kolom1")
             Text(text = "Komponen3Kolom1")
         }
+        //Kolom2
+        Column() {
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+            Text(text = "Komponen3Kolom2")
+        }
+    }
+}
 
